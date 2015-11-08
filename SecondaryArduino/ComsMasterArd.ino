aee50bc1527a31aee5b5d0158bbc8c1e348aa885
erickchen <ekchen35649@gmail.com>
2015-11-07 21:45:30 -0800

    Update ComsMasterArd.ino

--- SecondaryArduino/ComsMasterArd.ino
+++ SecondaryArduino/ComsMasterArd.ino
@@ -1,64 +1,36 @@
+#include <Wire.h>
 
-
-
-
-
-
-
-
-
-
-// Wire Slave Receiver
+// Wire Master Writer
 // by Nicholas Zambetti <http://www.zambetti.com>
 
 // Demonstrates use of the Wire library
-// Receives data as an I2C/TWI slave device
-// Refer to the "Wire Master Writer" example for use with this
+// Writes data to an I2C/TWI slave device
+// Refer to the "Wire Slave Receiver" example for use with this
 
 // Created 29 March 2006
 
 // This example code is in the public domain.
 
-//#include "gyroAccelerometer.h"
 
-#include <Wire.h>
-#include "pressure.h"
-#include "gyroAccelerometer.h"
-//
-Pressure Pressure;
-GyroAccelerometer Gyro;
-//GyroAccelerometer  GyroAccelerometer();
+
+
 void setup() {
-  Wire.begin(8);                // join i2c bus with address #8
-  Pressure.pressureSetup();
-  Pressure.updatePressureSensor();
-  //Gyro.accelSetup();
-  //Gyro.updateAccelRaw();
-  // Pressure Requests
-  Wire.onRequest(requestEventPressure); // register event
-  Wire.onRequest(requestEventDepth); // depth
-  Wire.onRequest(requestEventTemp); // temp
-  
+  Wire.begin(); // join i2c bus (address optional for master)
+  Serial.begin(9600);
 }
+
+byte x = 0;
 
 void loop() {
-  delay(100);
-}
-
-// function that executes whenever data is requested by master
-// this function is registered as an event, see setup()
-void requestEventPressure() {
-  int currentPressure = Pressure.getPressure();
-  Wire.write(currentPressure); // respond with message of 6 bytes
-  // as expected by master
-}
-void requestEventDepth() {
-  int currentDepth = Pressure.getDepth();
-  Wire.write(currentDepth); // respond with message of 6 bytes
-  // as expected by master
-}
-void requestEventTemp() {
-  int currentTemp = Pressure.getTempTimesTen();
-  Wire.write(currentTemp); // respond with message of 6 bytes
-  // as expected by master
+  Wire.beginTransmission(8); // transmit to device #8
+  Wire.write("x is ");        // sends five bytes
+  Wire.write(x);              // sends one byte
+  Wire.endTransmission();    // stop transmitting
+  Serial.println(x);
+  if(x<10){
+    x++; }
+    else {
+      x=0;
+    }
+  delay(500);
 }
