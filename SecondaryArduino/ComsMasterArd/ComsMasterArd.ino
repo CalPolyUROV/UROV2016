17712d4fa5bc13045db4b47ced2b805c7125d60c
erickchen <ekchen35649@gmail.com>
2015-11-14 14:53:23 -0800

    Update ComsMasterArd.ino

--- SecondaryArduino/ComsMasterArd/ComsMasterArd.ino
+++ SecondaryArduino/ComsMasterArd/ComsMasterArd.ino
@@ -19,18 +19,54 @@ void setup() {
   Serial.begin(9600);
 }
 
-byte x = 0;
-
+int x = 0;
+String sendData;
+String receivedString;
+int receivedInt;
+int comsIndex;
+char tempReceive;
 void loop() {
-  Wire.beginTransmission(8); // transmit to device #8
-  Wire.write("x is ");        // sends five bytes
-  Wire.write(x);              // sends one byte
-  Wire.endTransmission();    // stop transmitting
-  Serial.println(x);
-  if(x<10){
-    x++; }
+  masterWrite();
+  
+  delay(250);
+  
+  masterRequest();
+  
+  if(x<12000){
+    x+=765; }
     else {
       x=0;
     }
-  delay(500);
+    Serial.println("e");
+    Serial.println(x);
+  delay(250);
+}
+
+
+void masterWrite(){
+  Serial.println("s");
+  sendData = String(x+10000);
+  Serial.println(sendData);
+  Wire.beginTransmission(8); // transmit to device #8
+
+  Wire.write(sendData.c_str());
+
+  Wire.endTransmission();    // stop transmitting
+}
+
+void masterRequest(){
+  Serial.println("req");
+  receivedString = 0;
+  Wire.requestFrom(8,5);
+  while (Wire.available()) {
+    tempReceive = Wire.read();
+    receivedString += tempReceive;
+    
+  }
+  Serial.print(receivedString);
+  Serial.println("raw");
+  receivedInt = receivedString.toInt();
+  receivedInt -= 10000;
+  Serial.print(receivedInt);
+  Serial.println("fin");
 }
